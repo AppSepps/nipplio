c39cbae95be68bd2bf83a791cba1063f037f6aa6
Jan-Patrick Hespelt <Jan-Patrick.Hespelt@wuerth-it.com>
2021-06-25 08:42:22 +0200

    Added persisted state

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,11 +1,16 @@
 import { createStore, createLogger } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import app from './modules/app'
+
+const persistedAppState = createPersistedState({
+    paths: ['app'],
+})
 
 const store = createStore({
     modules: {
         app,
     },
-    plugins: [createLogger()],
+    plugins: [createLogger(), persistedAppState],
 })
 
 export default store
