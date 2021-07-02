36e5e2a01f0d2dbfdac0cf45029d031f6252ed2b
Lucien Kerl <lucien.kerl@wuerth-it.com>
2021-07-02 14:46:36 +0200

    fixed onUserCreate Bug

--- firebase/functions/onauth.js
+++ firebase/functions/onauth.js
@@ -2,7 +2,10 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 exports.createUserInDatabase = functions.auth.user().onCreate(async (user) => {
-  await admin.database.ref("/users/" + user.uid + "/").push({
-    displayName: user.displayName,
-  });
+  await admin
+    .database()
+    .ref("/users/" + user.uid + "/")
+    .update({
+      displayName: user.displayName,
+    });
 });
