2f6375b37504226be44c7330c77e2c0faf1d8f9d
Lucien Kerl <lucien.kerl@wuerth-it.com>
2021-07-16 10:31:12 +0200

    added account selection for google

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -15,7 +15,16 @@ export default {
             firebaseui.auth.AuthUI.getInstance() ||
             new firebaseui.auth.AuthUI(firebase.auth())
         const uiConfig = {
-            signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+            signInOptions: [
+                {
+                    provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                    customParameters: {
+                        // Forces account selection even when one account
+                        // is available.
+                        prompt: 'select_account',
+                    },
+                },
+            ],
         }
         this.ui.start('#firebaseui-auth-container', uiConfig)
     },
