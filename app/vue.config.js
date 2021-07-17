943e748365472e01329f5e8239fcc593f4817896
Lucien Kerl <lucien.kerl@wuerth-it.com>
2021-07-17 18:03:27 +0200

    changed target to electron-renderer

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     configureWebpack: {
         // Webpack configuration applied to web builds and the electron renderer process
-        target: process.env.WEBPACK_DEV_SERVER_URL !== null ? 'web' : 'electron-renderer',
-        //target: 'electron-renderer'
+        //target: process.env.WEBPACK_DEV_SERVER_URL !== null ? 'web' : 'electron-renderer',
+        target: 'electron-renderer'
         //target: 'web'
     },
     pluginOptions: {
