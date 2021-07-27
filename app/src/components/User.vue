2c5fb172892316a1a51da6e4dad80a8558e4924f
Jan-Patrick <janpatrick97@gmail.com>
2021-07-27 19:46:33 +0200

    Added user mute indicator and speaker icon

--- app/src/components/User.vue
+++ app/src/components/User.vue
@@ -1,8 +1,17 @@
 <template>
-    <q-item>
-        <q-item-section avatar :class="{ 'user-disabled': !user.connected }">
+    <q-item
+        @mouseover="hover = true"
+        @mouseleave="hover = false"
+        :clickable="user.connected"
+        :class="{ 'user-disabled': !user.connected }"
+    >
+        <q-item-section avatar>
             <q-avatar v-if="user.photoURL">
                 <img v-if="user.photoURL" :src="user.photoURL" />
+                <user-mute-indicator
+                    v-if="user.connected"
+                    :muted="user.muted"
+                />
             </q-avatar>
             <q-avatar
                 v-else
@@ -10,32 +19,33 @@
                 text-color="white"
             >
                 <div>{{ user.displayName[0].toUpperCase() }}</div>
+                <user-mute-indicator
+                    v-if="user.connected"
+                    :muted="user.muted"
+                />
             </q-avatar>
         </q-item-section>
-        <q-item-section :class="{ 'user-disabled': !user.connected }">
+        <q-item-section>
             <q-item-label>{{ user.displayName }}</q-item-label>
         </q-item-section>
-        <q-item-section
-            avatar
-            v-if="!isCurrentUser && user.connected"
-            :class="{ 'user-disabled': !user.connected }"
-        >
-            <q-icon
-                :name="user.muted ? 'headset_off' : 'headset'"
-                color="secondary"
-            />
-        </q-item-section>
-        <q-item-section
-            avatar
-            v-if="!isCurrentUser && user.connected"
-            :class="{ 'user-disabled': !user.connected }"
-        >
+        <q-item-section avatar v-if="hover && user.connected">
             <q-btn
                 unelevated
                 flat
                 round
-                :icon="muted ? 'music_off' : 'music_note'"
-                :color="muted ? 'red' : 'primary'"
+                icon="speaker"
+                :color="'primary'"
+                @click="onSelectSpeaker(user.id)"
+            />
+        </q-item-section>
+        <q-item-section avatar v-if="user.connected">
+            <q-btn
+                unelevated
+                flat
+                round
+                :disable="isCurrentUser"
+                :icon="muted ? 'volume_off' : 'volume_up'"
+                :color="muted ? 'red' : 'white'"
                 @click="onToggleUserMute(user.id)"
             />
         </q-item-section>
@@ -43,13 +53,23 @@
 </template>
 
 <script>
+import UserMuteIndicator from './UserMuteIndicator.vue'
+
 export default {
     name: 'User',
     props: ['user', 'muted', 'isCurrentUser'],
-    components: {},
+    components: { UserMuteIndicator },
+    data() {
+        return {
+            hover: false,
+        }
+    },
     methods: {
         async onToggleUserMute(id) {
             await this.$store.dispatch('app/toggleUserMute', { id })
+        },
+        async onSelectSpeaker(id) {
+            await this.$store.dispatch('app/selectSpeaker', { id })
         },
     },
 }
---
+++ app/src/components/UserMuteIndicator.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <q-avatar
+            color="dark"
+            class="absolute"
+            size="18px"
+            style="bottom: -5px; right: -5px"
+            ><q-badge :color="muted ? 'red' : 'primary'" rounded
+        /></q-avatar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserMuteIndicator',
+    props: ['muted'],
+}
+</script>
+
+<style></style>
