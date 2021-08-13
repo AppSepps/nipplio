8333d7bb63f01b5feaa121c5c63e4b63f2cfc4e8
Jan-Patrick Hespelt <Jan-Patrick.Hespelt@wuerth-it.com>
2021-08-13 16:12:49 +0200

    quick fix

--- app/src/components/SlotMappingDialog.vue
+++ app/src/components/SlotMappingDialog.vue
@@ -126,7 +126,6 @@ export default {
                 boardId: this.selectedBoard.id,
                 selectedSounds: this.selectedSounds,
             })
-            this.selectedSounds = {}
         },
     },
 }
