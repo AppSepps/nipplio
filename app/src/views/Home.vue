d76e199cdc5747e61b1391091722361deb95eaf9
Lucien Kerl <lucien.kerl@wuerth-it.com>
2021-10-15 09:58:43 +0200

    added ble button to settings

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,116 +1,116 @@
 <template>
-    <q-layout view="lHh LpR lFf" container style="height: 100vh">
-        <q-header class="shadow-1">
-            <q-toolbar class="bg-dark text-white">
-                <board-title-dropdown
-                    v-on:openInviteDialog="showBoardInviteDialog = true"
-                    v-on:openLeaveBoardDialog="showLeaveBoardDialog = true"
-                />
-                <q-space />
-                <search-bar ref="searchBar" v-if="activeBoard" />
-            </q-toolbar>
-        </q-header>
-        <board-select
-            v-on:openAddBoardDialog="showAddBoardDialog = true"
-            v-on:openCustomizeDialog="showCustomizeDialog = true"
-            v-on:openSettingsDialog="showSettingsModal = true"
+  <q-layout view="lHh LpR lFf" container style="height: 100vh">
+    <q-header class="shadow-1">
+      <q-toolbar class="bg-dark text-white">
+        <board-title-dropdown
+            v-on:openInviteDialog="showBoardInviteDialog = true"
+            v-on:openLeaveBoardDialog="showLeaveBoardDialog = true"
         />
-        <user-drawer v-if="activeBoard" />
-        <q-page-container>
-            <q-page padding>
-                <div v-if="!activeBoard">
-                    You are currently not connected with any board. Select a
-                    board on the left side or create a new board.
-                </div>
-                <div v-else>
-                    <!-- TODO: Extract sound table in extra component -->
-                    <q-table
-                        class="sound-table"
-                        virtual-scroll
-                        flat
-                        separator="none"
-                        :rows-per-page-options="[0]"
-                        :rows="filteredSounds"
-                        :columns="columns"
-                        row-key="id"
-                        hide-bottom
-                    >
-                        <template v-slot:top>
-                            <div class="text-h6 text-bold">
-                                Sounds ({{ filteredSounds.length }})
-                            </div>
-                            <q-space />
-                            <q-item v-if="availableTags.length > 0">
-                                <q-chip
-                                    v-for="tag in availableTags"
-                                    clickable
-                                    :key="tag"
-                                    :selected="isSelected(tag)"
-                                    @click="onClickTag(tag)"
-                                    color="secondary"
-                                    text-color="white"
-                                >
-                                    {{ tag }}
-                                </q-chip>
-                            </q-item>
-                        </template>
-                        <template v-slot:header="props">
-                            <q-tr :props="props">
-                                <q-th auto-width />
-                                <q-th
-                                    v-for="col in props.cols"
-                                    :key="col.name"
-                                    :props="props"
-                                >
-                                    {{ col.label }}
-                                </q-th>
-                                <q-th auto-width />
-                            </q-tr>
-                        </template>
-                        <template v-slot:body="props">
-                            <sound
-                                :key="props.row.id"
-                                :props="props"
-                                :sound="props.row"
-                                :user="
+        <q-space/>
+        <search-bar ref="searchBar" v-if="activeBoard"/>
+      </q-toolbar>
+    </q-header>
+    <board-select
+        v-on:openAddBoardDialog="showAddBoardDialog = true"
+        v-on:openCustomizeDialog="showCustomizeDialog = true"
+        v-on:openSettingsDialog="showSettingsModal = true"
+    />
+    <user-drawer v-if="activeBoard"/>
+    <q-page-container>
+      <q-page padding>
+        <div v-if="!activeBoard">
+          You are currently not connected with any board. Select a
+          board on the left side or create a new board.
+        </div>
+        <div v-else>
+          <!-- TODO: Extract sound table in extra component -->
+          <q-table
+              class="sound-table"
+              virtual-scroll
+              flat
+              separator="none"
+              :rows-per-page-options="[0]"
+              :rows="filteredSounds"
+              :columns="columns"
+              row-key="id"
+              hide-bottom
+          >
+            <template v-slot:top>
+              <div class="text-h6 text-bold">
+                Sounds ({{ filteredSounds.length }})
+              </div>
+              <q-space/>
+              <q-item v-if="availableTags.length > 0">
+                <q-chip
+                    v-for="tag in availableTags"
+                    clickable
+                    :key="tag"
+                    :selected="isSelected(tag)"
+                    @click="onClickTag(tag)"
+                    color="secondary"
+                    text-color="white"
+                >
+                  {{ tag }}
+                </q-chip>
+              </q-item>
+            </template>
+            <template v-slot:header="props">
+              <q-tr :props="props">
+                <q-th auto-width/>
+                <q-th
+                    v-for="col in props.cols"
+                    :key="col.name"
+                    :props="props"
+                >
+                  {{ col.label }}
+                </q-th>
+                <q-th auto-width/>
+              </q-tr>
+            </template>
+            <template v-slot:body="props">
+              <sound
+                  :key="props.row.id"
+                  :props="props"
+                  :sound="props.row"
+                  :user="
                                     boardUsers.filter(
                                         (u) => u.id === props.row.createdBy
                                     )[0]
                                 "
-                                v-on:openRemoveDialog="
+                  v-on:openRemoveDialog="
                                     showRemoveSoundDialog = true
                                 "
-                                v-on:openEditDialog="showEditSoundDialog = true"
-                                v-on:openInfoDialog="showSoundInfoDialog = true"
-                            />
-                        </template>
-                    </q-table>
-                </div>
-            </q-page>
-        </q-page-container>
-        <q-footer v-if="activeBoard">
-            <audio-player />
-        </q-footer>
-        <settings
-            v-model="showSettingsModal"
-            v-on:openSlotMappingDialog="showSlotMappingDialog = true"
-        />
-        <add-board-dialog
-            v-model="showAddBoardDialog"
-            v-on:closeDialog="showAddBoardDialog = false"
-        />
-        <board-invite-dialog v-model="showBoardInviteDialog" />
-        <remove-sound-dialog v-model="showRemoveSoundDialog" />
-        <edit-sound-dialog v-model="showEditSoundDialog" />
-        <sound-info-dialog v-model="showSoundInfoDialog" />
-        <slot-mapping-dialog v-model="showSlotMappingDialog" />
-        <customize-dialog v-model="showCustomizeDialog" />
-        <leave-board-dialog v-model="showLeaveBoardDialog" />
-    </q-layout>
+                  v-on:openEditDialog="showEditSoundDialog = true"
+                  v-on:openInfoDialog="showSoundInfoDialog = true"
+              />
+            </template>
+          </q-table>
+        </div>
+      </q-page>
+    </q-page-container>
+    <q-footer v-if="activeBoard">
+      <audio-player/>
+    </q-footer>
+    <settings
+        v-model="showSettingsModal"
+        v-on:openSlotMappingDialog="showSlotMappingDialog = true"
+    />
+    <add-board-dialog
+        v-model="showAddBoardDialog"
+        v-on:closeDialog="showAddBoardDialog = false"
+    />
+    <board-invite-dialog v-model="showBoardInviteDialog"/>
+    <remove-sound-dialog v-model="showRemoveSoundDialog"/>
+    <edit-sound-dialog v-model="showEditSoundDialog"/>
+    <sound-info-dialog v-model="showSoundInfoDialog"/>
+    <slot-mapping-dialog v-model="showSlotMappingDialog"/>
+    <customize-dialog v-model="showCustomizeDialog"/>
+    <leave-board-dialog v-model="showLeaveBoardDialog"/>
+  </q-layout>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import {mapGetters, mapState} from 'vuex'
 import Settings from './Settings.vue'
 import Sound from '../components/Sound.vue'
 import AddBoardDialog from '../components/AddBoardDialog.vue'
@@ -128,108 +128,108 @@ import BoardTitleDropdown from '../components/BoardTitleDropdown.vue'
 import LeaveBoardDialog from '../components/LeaveBoardDialog.vue'
 
 const columns = [
-    {
-        name: 'name',
-        required: true,
-        label: 'Name',
-        align: 'left',
-        field: (row) => row.name,
-        sortable: true,
-    },
-    {
-        name: 'tags',
-        sortable: false,
-    },
-    {
-        name: 'createdAt',
-        required: true,
-        label: 'Created at',
-        field: (row) => row.createdAt,
-        sortable: true,
-    },
+  {
+    name: 'name',
+    required: true,
+    label: 'Name',
+    align: 'left',
+    field: (row) => row.name,
+    sortable: true,
+  },
+  {
+    name: 'tags',
+    sortable: false,
+  },
+  {
+    name: 'createdAt',
+    required: true,
+    label: 'Created at',
+    field: (row) => row.createdAt,
+    sortable: true,
+  },
 ]
 
 export default {
-    name: 'Home',
-    components: {
-        Settings,
-        Sound,
-        AddBoardDialog,
-        AudioPlayer,
-        BoardInviteDialog,
-        RemoveSoundDialog,
-        SearchBar,
-        EditSoundDialog,
-        SoundInfoDialog,
-        UserDrawer,
-        SlotMappingDialog,
-        BoardSelect,
-        CustomizeDialog,
-        BoardTitleDropdown,
-        LeaveBoardDialog,
+  name: 'Home',
+  components: {
+    Settings,
+    Sound,
+    AddBoardDialog,
+    AudioPlayer,
+    BoardInviteDialog,
+    RemoveSoundDialog,
+    SearchBar,
+    EditSoundDialog,
+    SoundInfoDialog,
+    UserDrawer,
+    SlotMappingDialog,
+    BoardSelect,
+    CustomizeDialog,
+    BoardTitleDropdown,
+    LeaveBoardDialog,
+  },
+  data() {
+    return {
+      showSettingsModal: false,
+      showAddBoardDialog: false,
+      showBoardInviteDialog: false,
+      showRemoveSoundDialog: false,
+      showEditSoundDialog: false,
+      showSoundInfoDialog: false,
+      showSlotMappingDialog: false,
+      showCustomizeDialog: false,
+      showLeaveBoardDialog: false,
+      columns,
+    }
+  },
+  methods: {
+    onUpdateTag: function (state) {
+      console.log('onUpdateTag', state)
     },
-    data() {
-        return {
-            showSettingsModal: false,
-            showAddBoardDialog: false,
-            showBoardInviteDialog: false,
-            showRemoveSoundDialog: false,
-            showEditSoundDialog: false,
-            showSoundInfoDialog: false,
-            showSlotMappingDialog: false,
-            showCustomizeDialog: false,
-            showLeaveBoardDialog: false,
-            columns,
-        }
+    onClickTag: function (tagName) {
+      return this.$store.dispatch('sound/onTagClicked', {
+        tagName,
+      })
     },
-    methods: {
-        onUpdateTag: function (state) {
-            console.log('onUpdateTag', state)
-        },
-        onClickTag: function (tagName) {
-            return this.$store.dispatch('sound/onTagClicked', {
-                tagName,
-            })
-        },
-        isSelected: function (tagName) {
-            return this.$store.state.sound.selectedTags.includes(tagName)
-        },
+    isSelected: function (tagName) {
+      return this.$store.state.sound.selectedTags.includes(tagName)
     },
-    computed: {
-        selectedTags: {
-            get() {
-                return this.$store.state.sound.selectedTags
-            },
-            set(value) {
-                return value
-            },
-        },
-        ...mapGetters('sound', ['filteredSounds', 'availableTags']),
-        ...mapState({
-            activeBoard: (state) => state.board.activeBoard,
-            boardUsers: (state) => state.user.boardUsers,
-            sounds: (state) => state.sound.sounds,
-            user: (state) => state.user.user,
-        }),
+  },
+  computed: {
+    selectedTags: {
+      get() {
+        return this.$store.state.sound.selectedTags
+      },
+      set(value) {
+        return value
+      },
     },
-    async mounted() {
-        const that = this
-        this.$store.dispatch('board/registerShortcuts', function () {
-            console.log('should focus')
-            that.$refs.searchBar.focus()
-        })
-        await this.$store.dispatch('board/checkForInviteLinkInUrl')
-        await this.$store.dispatch('board/getBoards')
-        await this.$store.dispatch('user/getUser')
-        await this.$store.dispatch('user/getBoardUsers')
-        await this.$store.dispatch('user/updateConnectionStatus')
-        await this.$store.dispatch('sound/getSounds')
-        await this.$store.dispatch('player/unsubscribeToPlayer')
-        await this.$store.dispatch('player/subscribeToPlayer')
-        await this.$store.dispatch('player/subscribeToRemotePlayer')
-        await this.$store.dispatch('settings/subscribeToRemoteDevices')
-        await this.$store.dispatch('board/getApiKeys')
-    },
+    ...mapGetters('sound', ['filteredSounds', 'availableTags']),
+    ...mapState({
+      activeBoard: (state) => state.board.activeBoard,
+      boardUsers: (state) => state.user.boardUsers,
+      sounds: (state) => state.sound.sounds,
+      user: (state) => state.user.user,
+    }),
+  },
+  async mounted() {
+    const that = this
+    this.$store.dispatch('board/registerShortcuts', function () {
+      console.log('should focus')
+      that.$refs.searchBar.focus()
+    })
+    await this.$store.dispatch('board/checkForInviteLinkInUrl')
+    await this.$store.dispatch('board/getBoards')
+    await this.$store.dispatch('user/getUser')
+    await this.$store.dispatch('user/getBoardUsers')
+    await this.$store.dispatch('user/updateConnectionStatus')
+    await this.$store.dispatch('sound/getSounds')
+    await this.$store.dispatch('player/unsubscribeToPlayer')
+    await this.$store.dispatch('player/subscribeToPlayer')
+    await this.$store.dispatch('player/subscribeToRemotePlayer')
+    await this.$store.dispatch('settings/subscribeToRemoteDevices')
+    await this.$store.dispatch('board/getApiKeys')
+  },
 }
 </script>
 
--- app/src/views/Settings.vue
+++ app/src/views/Settings.vue
@@ -1,162 +1,178 @@
 <template>
-    <q-dialog>
-        <q-card style="width: 800px; max-width: 80vw">
-            <q-card-section class="row items-center">
-                <div class="text-h6">Settings</div>
-                <q-space />
-                <q-btn icon="close" flat round dense v-close-popup />
-            </q-card-section>
-            <q-card-section>
-                <!--<div class="row q-pb-sm">
-                    <q-list class="col-6">
-                        <q-item-label header>Desktop App Settings</q-item-label>
-                        <q-item clickable>
-                            <q-item-section>
-                                <q-item-label>Nothing here yet</q-item-label>
-                            </q-item-section>
-                        </q-item>
-                        <q-separator spaced />
-                    </q-list>
-                </div>-->
-                <div v-if="isOwner" class="row q-pb-sm">
-                    <q-list class="col-12">
-                        <q-item>
-                            <q-item-section>
-                                <q-item-label header>API Keys </q-item-label>
-                            </q-item-section>
-                            <q-item-section avatar>
-                                <q-btn
-                                    no-caps
-                                    @click="addApiKey"
-                                    color="primary"
-                                    icon="add"
-                                    label="New"
-                                />
-                            </q-item-section>
-                        </q-item>
-                        <q-item v-for="apiKey in apiKeys" :key="apiKey">
-                            <q-item-section>
-                                <q-item-label>{{ apiKey }}</q-item-label>
-                            </q-item-section>
-                            <q-item-section avatar>
-                                <q-btn
-                                    flat
-                                    round
-                                    color="secondary"
-                                    icon="assignment"
-                                    @click="copyToClipboard(apiKey)"
-                                />
-                            </q-item-section>
-                            <q-item-section avatar>
-                                <q-btn
-                                    flat
-                                    round
-                                    color="red"
-                                    icon="delete"
-                                    @click="deleteApiKey(apiKey)"
-                                />
-                            </q-item-section>
-                        </q-item>
-                        <q-separator spaced />
-                    </q-list>
-                </div>
-                <!--<div class="row q-pb-sm">
-                    <q-list class="col-6">
-                        <q-item-label header v-if="remoteDevices.length > 0"
-                            >Linked Devices -
-                            {{ remoteDevices.length }}</q-item-label
-                        >
-                        <remote-device
-                            v-for="(device, index) in remoteDevices"
-                            :key="index"
-                            :device="device"
-                            :linked="true"
-                            v-on:openDialog="openSlotMappingDialog()"
-                        />
-                        <q-item-label header
-                            >Discovered Devices -
-                            {{ filteredDiscoveredDevices.length }}</q-item-label
-                        >
-                        <remote-device
-                            v-for="(device, index) in filteredDiscoveredDevices"
-                            :key="index"
-                            :device="device"
-                            :linked="false"
-                        />
-                        <q-item v-if="filteredDiscoveredDevices.length === 0">
-                            <q-item-section>
-                                <q-item-label
-                                    >No devices discovered...</q-item-label
-                                >
-                            </q-item-section>
-                        </q-item>
-                    </q-list>
-                </div>
+  <q-dialog>
+    <q-card style="width: 800px; max-width: 80vw">
+      <q-card-section class="row items-center">
+        <div class="text-h6">Settings</div>
+        <q-space/>
+        <q-btn icon="close" flat round dense v-close-popup/>
+      </q-card-section>
+      <q-card-section>
+        <!--<div class="row q-pb-sm">
+            <q-list class="col-6">
+                <q-item-label header>Desktop App Settings</q-item-label>
+                <q-item clickable>
+                    <q-item-section>
+                        <q-item-label>Nothing here yet</q-item-label>
+                    </q-item-section>
+                </q-item>
+                <q-separator spaced />
+            </q-list>
+        </div>-->
+        <div v-if="!isElectron" class="row q-pb-sm">
+          <q-list class="col-12">
+            <q-item>
+              <q-item-section>
+                <q-item-label header>Bluetooth Board</q-item-label>
+              </q-item-section>
+              <q-item-section avatar>
                 <q-btn
                     no-caps
                     unelevated
                     color="primary"
-                    label="Pair Bluetooth Board"
+                    label="Connect"
                     @click="bleButtonScanClicked"
-                />-->
+                />
+              </q-item-section>
+            </q-item>
+            <q-separator spaced/>
+          </q-list>
+        </div>
+        <div v-if="isOwner" class="row q-pb-sm">
+          <q-list class="col-12">
+            <q-item>
+              <q-item-section>
+                <q-item-label header>API Keys</q-item-label>
+              </q-item-section>
+              <q-item-section avatar>
                 <q-btn
                     no-caps
-                    unelevated
-                    color="red"
-                    label="Sign out"
-                    @click="signOut"
+                    @click="addApiKey"
+                    color="primary"
+                    icon="add"
+                    label="New"
                 />
-            </q-card-section>
-        </q-card>
-    </q-dialog>
+              </q-item-section>
+            </q-item>
+            <q-item v-for="apiKey in apiKeys" :key="apiKey">
+              <q-item-section>
+                <q-item-label>{{ apiKey }}</q-item-label>
+              </q-item-section>
+              <q-item-section avatar>
+                <q-btn
+                    flat
+                    round
+                    color="secondary"
+                    icon="assignment"
+                    @click="copyToClipboard(apiKey)"
+                />
+              </q-item-section>
+              <q-item-section avatar>
+                <q-btn
+                    flat
+                    round
+                    color="red"
+                    icon="delete"
+                    @click="deleteApiKey(apiKey)"
+                />
+              </q-item-section>
+            </q-item>
+            <q-separator spaced/>
+          </q-list>
+        </div>
+        <!--<div class="row q-pb-sm">
+            <q-list class="col-6">
+                <q-item-label header v-if="remoteDevices.length > 0"
+                    >Linked Devices -
+                    {{ remoteDevices.length }}</q-item-label
+                >
+                <remote-device
+                    v-for="(device, index) in remoteDevices"
+                    :key="index"
+                    :device="device"
+                    :linked="true"
+                    v-on:openDialog="openSlotMappingDialog()"
+                />
+                <q-item-label header
+                    >Discovered Devices -
+                    {{ filteredDiscoveredDevices.length }}</q-item-label
+                >
+                <remote-device
+                    v-for="(device, index) in filteredDiscoveredDevices"
+                    :key="index"
+                    :device="device"
+                    :linked="false"
+                />
+                <q-item v-if="filteredDiscoveredDevices.length === 0">
+                    <q-item-section>
+                        <q-item-label
+                            >No devices discovered...</q-item-label
+                        >
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </div>
+        -->
+        <q-btn
+            no-caps
+            unelevated
+            color="red"
+            label="Sign out"
+            @click="signOut"
+        />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
-import { sendToIPCRenderer } from '../helpers/electron.helper'
+import {isElectron, sendToIPCRenderer} from '../helpers/electron.helper'
 import firebase from 'firebase'
-import { mapActions, mapGetters } from 'vuex'
+import {mapActions, mapGetters} from 'vuex'
 // import RemoteDevice from '../components/RemoteDevice.vue'
-import { copyToClipboard } from 'quasar'
+import {copyToClipboard} from 'quasar'
 
 export default {
-    name: 'Settings',
-    components: {
-        /*RemoteDevice*/
+  name: 'Settings',
+  components: {
+    /*RemoteDevice*/
+  },
+  created() {
+    this.copyToClipboard = copyToClipboard
+  },
+  computed: {
+    ...mapGetters('settings', [
+      'filteredDiscoveredDevices',
+      'isOwner',
+      'remoteDevices',
+      'apiKeys',
+    ]),
+    isElectron: function () {
+      return isElectron
+    }
+  },
+  methods: {
+    ...mapActions('settings', [
+      'bleButtonScanClicked',
+      'addApiKey',
+      'deleteApiKey',
+    ]),
+    openSlotMappingDialog: function () {
+      this.$emit('openSlotMappingDialog')
     },
-    created() {
-        this.copyToClipboard = copyToClipboard
+    signOut: async function () {
+      await firebase.auth().signOut()
+      this.$store.dispatch('player/unsubscribeToPlayer')
+      this.$store.dispatch('clearAll')
+      this.$router.push('/welcome')
     },
-    computed: {
-        ...mapGetters('settings', [
-            'filteredDiscoveredDevices',
-            'isOwner',
-            'remoteDevices',
-            'apiKeys',
-        ]),
-    },
-    methods: {
-        ...mapActions('settings', [
-            'bleButtonScanClicked',
-            'addApiKey',
-            'deleteApiKey',
-        ]),
-        openSlotMappingDialog: function () {
-            this.$emit('openSlotMappingDialog')
-        },
-        signOut: async function () {
-            await firebase.auth().signOut()
-            this.$store.dispatch('player/unsubscribeToPlayer')
-            this.$store.dispatch('clearAll')
-            this.$router.push('/welcome')
-        },
-    },
-    async mounted() {
-        sendToIPCRenderer('startScanForDevices')
-        await this.$store.dispatch('settings/autoConnect')
-    },
-    unmounted() {
-        sendToIPCRenderer('stopScanForDevices')
-    },
+  },
+  async mounted() {
+    sendToIPCRenderer('startScanForDevices')
+    await this.$store.dispatch('settings/autoConnect')
+  },
+  unmounted() {
+    sendToIPCRenderer('stopScanForDevices')
+  },
 }
 </script>
 
